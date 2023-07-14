171b1b6ace8eb259a203afa695b4bde7a329071a
junleeca <jun.lee.ca@outlook.com>
2023-07-15 01:37:55 +0800

    Initialize maui

---
+++ src/Shared/BlazorBoilerplate.Shared/Models/Tokens.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBoilerplate.Shared.Models
+{
+    public class Tokens
+    {
+        public DateTimeOffset AccessTokenExpiration { get; set; }
+        public string IdentityToken { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
